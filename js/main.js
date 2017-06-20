d891c8bc5429a952d5fba984a1e0f8b92e938c11
megadevice83 <megadevice83@gmail.com>
2017-06-20 16:09:05 +0300

    - add go button and set animation for them

--- js/main.js
+++ js/main.js
@@ -2,14 +2,18 @@
  * Created by Oshevchuk on 20.06.2017.
  */
 
-
-
-
 (function () {
     $( document ).ready(function() {
 
+        $('.intro-content a').delay(500).animate({opacity: '1'}, 400, function () {
+            $(this).addClass('anim-button');//.delay(800).animate({});
+
+
+            $('.intro-content').addClass('anim-zone');
+        });
+
         $(window).on('resize', function() {
-            
+
         });
 
     });
