a6efc6cbb495f11742734bfc9f53d4fec23cf1bd
megadevice83 <megadevice83@gmail.com>
2017-06-20 13:40:57 +0300

    - init projects - set background video (style and content)

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Oshevchuk on 20.06.2017.
+ */
+
+
+
+
+(function () {
+    $( document ).ready(function() {
+
+        $(window).on('resize', function() {
+            
+        });
+
+    });
+
+
+})();
